632637adb82af6ca4e9ab80b53fcaea3fcb6ca08
Santiago Lo Coco <santilococo.01@gmail.com>
2020-11-19 15:02:54 -0300

    Added possibility for modifying the velocity of the scrolling

--- config.def.h
+++ config.def.h
@@ -171,14 +171,16 @@ static uint forcemousemod = ShiftMask;
 /*
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
+ * Button2: Click with the mouse wheel! For pasting!
  */
+const unsigned int mousescrollincrement = 3;
 static MouseShortcut mshortcuts[] = {
-	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	/* mask                 button		function        argument       release */
+	{ XK_ANY_MOD,		Button4,	kscrollup,      {.i =  mousescrollincrement} },
+	{ XK_ANY_MOD,		Button5,	kscrolldown,    {.i =  mousescrollincrement} },
+	{ XK_ANY_MOD,		Button2,	selpaste,	{.i =  0},	1 },
+	{ XK_ANY_MOD,           Button4,	ttysend,        {.s = "\031"} },
+	{ XK_ANY_MOD,           Button5,	ttysend,        {.s = "\005"} },
 };
 
 /* Internal keyboard shortcuts. */
